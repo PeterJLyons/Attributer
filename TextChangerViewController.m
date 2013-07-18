3eeb836b826ac4c2c403ce4f31d08a3e505a3962
PeterJLyons <lyoncub1028@gmail.com>
2013-07-17 20:50:37 -0400

    Attributer
    Start of a program to mess around with attributed strings.

---
+++ TextChangerViewController.m
@@ -0,0 +1,30 @@
+//
+//  TextChangerViewController.m
+//  Attributer
+//
+//  Created by Peter Lyons on 7/15/13.
+//  Copyright (c) 2013 Peter Lyons. All rights reserved.
+//
+
+#import "TextChangerViewController.h"
+
+@interface TextChangerViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *textInput;
+@property (strong, nonatomic) IBOutlet UILabel *textOutput;
+@property (strong, nonatomic) IBOutlet UIButton *updateButton;
+
+@end
+
+@implementation TextChangerViewController
+
+
+- (void)updateText:(NSAttributedString*)input
+{
+    [self.textOutput setAttributedText:input];
+}
+- (IBAction)updateDisplayText:(UIButton *)sender
+{
+    [self updateText:self.textInput.attributedText];
+}
+
+@end
